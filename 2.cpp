850434d0466c22f0ef2370d03a3526901409eaf0
MGDASH <68458513+MGDASH@users.noreply.github.com>
2021-05-10 21:27:48 -0700

    first C++
    first C++ code

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+	int x =15; //int ->> integer buyu buhel too
+	int y = 20; 
+	
+	 x=50;
+	cout<<"x="<<x<<endl<<"y="<<y;
+	
+	
+	
+	return 0;
+	
+}
---
+++ 3.cpp
@@ -0,0 +1,20 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+	int x;
+	int y;
+	int sum;
+	
+	
+	cout<<"Enter a number: ";
+	cin>>x;
+	cout<<"Enter another number: ";
+	cin>>y;
+	sum = x + y;
+	cout<<"Sum of two number is "<<sum;
+	
+	return 0;
+	
+}
