ce2626f1f44caa27a360d7c8b96b8efaf7c8c961
MGDASH <68458513+MGDASH@users.noreply.github.com>
2021-05-14 00:08:14 -0700

    home work if else #5
    home work if else #5

---
+++ home work if else #5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int too;
+	cout<< "Enter your number: ";
+	cin>> too;
+	
+	if(too % 2 == 0){
+		cout<<too<<" is even number";
+		
+	}else{
+		cout<<too<< " is odd number";
+	}
+	
+	
+	
+	
+	return 0;
+}
+
